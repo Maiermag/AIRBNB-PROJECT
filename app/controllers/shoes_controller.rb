577df7ca125301a55280cbe0a1503e3f25685667
maiermag <maiermag@gmail.com>
2021-02-23 16:25:30 +0100

    commit index and show pages

--- app/controllers/shoes_controller.rb
+++ app/controllers/shoes_controller.rb
@@ -1,9 +1,12 @@
 class ShoesController < ApplicationController
+  skip_before_action :authenticate_user!, only: [:index, :show]
+
   def index
     @shoes = Shoe.all
   end
 
   def show
+    @shoe = Shoe.find(params[:id])
   end
 
   def edit

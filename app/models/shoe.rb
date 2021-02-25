e6d0a0f5b2fc09b894268d7c7b5ccf837689ed1a
ry houllis <ry.houllis@gmail.com>
2021-02-25 16:30:51 +0100

    flatpickr calender inserted on show page

--- app/models/shoe.rb
+++ app/models/shoe.rb
@@ -2,10 +2,4 @@ class Shoe < ApplicationRecord
   belongs_to :user
   has_one_attached :photo
   has_many :bookings, dependent: :destroy
-
-  def unavailable_dates
-    bookings.pluck(:start_date, :end_date).map do |range|
-      { from: range[0], to: range[1] }
-    end
-  end
 end

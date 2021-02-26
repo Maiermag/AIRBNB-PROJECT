3b8b9f21820cc33747c36cfc6a4d92776196e69c
ry houllis <ry.houllis@gmail.com>
2021-02-26 14:12:32 +0100

    finalizing flatpickr calendar - Booking policy required to book dates.

--- app/javascript/plugins/flatpickr.js
+++ app/javascript/plugins/flatpickr.js
@@ -1,19 +1,31 @@
+// import flatpickr from "flatpickr";
+// import rangePlugin from "flatpickr/dist/plugins/rangePlugin";
+
+
+// const initFlatpickr = () => {
+//   flatpickr(".datepicker",{
+//     // altInput: true,
+//     // mode: "range",
+//     altInputClass: "invisible",
+//     minDate: "today",
+//     position: "above",
+//     inline: true,
+//     // "plugins": [new rangePlugin({ input: "#end_date"})]
+//   });
+// }
+
+
+
+// export { initFlatpickr }
+
 import flatpickr from "flatpickr";
 import rangePlugin from "flatpickr/dist/plugins/rangePlugin";
-
-
 const initFlatpickr = () => {
   flatpickr(".datepicker",{
-    // altInput: true,
-    // mode: "range",
+    mode: "range",
     altInputClass: "invisible",
     minDate: "today",
-    position: "above",
     inline: true,
-    // "plugins": [new rangePlugin({ input: "#end_date"})]
   });
 }
-
-
-
 export { initFlatpickr }
